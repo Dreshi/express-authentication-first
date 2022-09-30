3ec7d69d38b7b7e1815d86c7065a198d07057a30
Dreshi <73090979+Dreshi@users.noreply.github.com>
2022-10-01 10:23:52 +1300

    Registering

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+mongoose.connect('mongodb://127.0.0.1:27017/authDemo')
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!")
+        console.log(err)
+    })
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('THIS IS THE HOME PAGE!')
+})
+
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    // check whether it is working - ultimatley you never want to do this
+    // res.send(req.body)
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    // Checking whether the encryption worked
+    // res.send(hash);
+    res.redirect('/')
 })
 
 app.get('/secret', (req, res) => {
