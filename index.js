1aa5e122231e9b166cdf2569fa4952b374f7c969
Dreshi <73090979+Dreshi@users.noreply.github.com>
2022-10-01 11:32:10 +1300

    Logout

--- index.js
+++ index.js
@@ -68,11 +68,21 @@ app.post('/login', async (req, res) => {
     }
 })
 
+app.post('/logout', (req, res) => {
+    // is usually enough
+    // req.session.user_id = null;
+    // destroys the whole session id
+    req.session.destroy();
+    res.redirect('/login');
+})
+
 app.get('/secret', (req, res) => {
     if (!req.session.user_id) {
-        res.redirect('/login')
+        return res.redirect('/login')
     }
-    res.send('THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN!')
+    // for testing it works first
+    // res.send('THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN!')
+    res.render('secret')
 })
 
 app.listen(3000, () => {
